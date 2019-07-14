aea4807a5b2d343f9d3a85eb0690b5917488f4bb
shariq hussain <shariqsb93@gmail.com>
2019-07-14 08:38:25 +0500

    initial comit

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {View} from 'react-native'
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Text, } from 'native-base';
+import { Constants } from 'expo';
+import Drawer from 'react-native-drawer'
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import Sidebar from './Sidebar'
+export default class HeaderIconButtonTextButtonExample extends Component {
+  render() {
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+    return (
+      <Drawer
+      type="overlay"
+      tapToClose={true}
+      openDrawerOffset={0.2} // 20% gap on the right side of drawer
+      panCloseMask={0.5}
+      captureGestures={true}
+      closedDrawerOffset={-1}
+      styles={{
+        drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3 },
+        main: { paddingLeft: 3 },
+      }}
+      tweenHandler={(ratio) => ({
+        main: { opacity: (2 - ratio) / 2 }
+      })}
+
+      ref={(ref) => this._drawer = ref}
+      content={<Sidebar   />}
+    >
+    <GestureRecognizer
+    
+  
+    onSwipeRight={(state) => this._drawer.open() }
+  
+    config={config}
+    style={{
+      flex: 1,
+
+    }}
+    >
+
+
+
+
+
+
+    <Container>
+      <View style={{
+        backgroundColor: "#329111",
+        height: Constants.statusBarHeight,
+      }} />
+      <Header style={{ backgroundColor: '#329111', marginLeft: -5 }}>
+        <Left>
+          <Button transparent onPress={()=>{
+            this._drawer.open()
+          }}>
+            <Icon name='menu' style={{color:'#fff'}} />
+          </Button>
+        </Left>
+        <Body style={{flex:1}}>
+          <Text style={{ fontFamily: 'f_light', color: '#fff', fontSize: 18 }}>Main Menu</Text>
+        </Body>
+        <Right>
+          
+        </Right>
+      </Header>
+    
+          <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Home Content</Text>
+          </View>
+
+    </Container>
+
+    </GestureRecognizer>
+ </Drawer>
+    );
+  }
+
+}
---
+++ Sidebar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import {  Container, View, Text,  } from "native-base";
+
+
+export default class Router extends Component {
+
+  constructor() {
+    super();
+   
+
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  componentWillMount() {
+
+   
+
+
+  }
+  render() {
+   
+    return (
+
+
+
+      <Container style={{backgroundColor:'#95a5a6'}}>
+       
+
+       <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Side Drawer</Text>
+          </View>
+
+
+      
+
+      </Container>
+
+
+
+    );
+  }
+}
