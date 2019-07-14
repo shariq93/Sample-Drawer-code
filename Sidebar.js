a164878ca8d3a479498ac8107da8539a9313552f
shariq hussain <shariqsb93@gmail.com>
2019-07-14 10:41:44 +0500

    Name correction

--- Sidebar.js
+++ Sidebar.js
@@ -1,47 +1,13 @@
 import React, { Component } from "react";
-
 import {  Container, View, Text,  } from "native-base";
-
-
-export default class Router extends Component {
-
-  constructor() {
-    super();
-   
-
-  }
-
-  componentDidMount() {
-
-  }
-
-
-  componentWillMount() {
-
-   
-
-
-  }
+export default class Sidebar extends Component {
   render() {
-   
     return (
-
-
-
       <Container style={{backgroundColor:'#95a5a6'}}>
-       
-
        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
             <Text>Side Drawer</Text>
           </View>
-
-
-      
-
       </Container>
-
-
-
     );
   }
 }
